49b17fa92c29fdd75d9fbf4b6bace06acdad32cf
Ramith Nambiar <ramith.rm@gmail.com>
2025-05-08 15:44:30 +0530

    Updates

--- docker/init.sh
+++ docker/init.sh
@@ -24,7 +24,7 @@ bench set-redis-socketio-host redis:6379
 sed -i '/redis/d' ./Procfile
 sed -i '/watch/d' ./Procfile
 
-bench get-app lms
+bench get-app lms https://github.com/ramith27/lms
 
 bench new-site lms.localhost \
 --force \
