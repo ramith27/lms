ee5c3ad1b70d4b6805c7171958e10e9d481637a1
Ramith Nambiar <ramith.rm@gmail.com>
2025-05-18 17:14:07 +0530

    Updates

--- frontend/src/components/AppSidebar.vue
+++ frontend/src/components/AppSidebar.vue
@@ -1,6 +1,6 @@
 <template>
 	<div
-		class="flex h-full flex-col justify-between transition-all duration-300 ease-in-out"
+		class="flex h-full flex-col justify-between transition-all duration-300 ease-in-out border-r"
 		:class="sidebarStore.isSidebarCollapsed ? 'w-14' : 'w-56'"
 	>
 		<div
